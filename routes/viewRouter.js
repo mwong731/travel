d5094a6d6c13457902aa7545bbeb6db666b94887
ynmwong <melodysweet@gmail.com>
2018-12-04 18:03:19 +0800

    added city hb

--- routes/viewRouter.js
+++ routes/viewRouter.js
@@ -5,7 +5,7 @@ module.exports = class ViewRouter{
     router(){
         const router = express.Router();
         router.get('/',(req,res)=>res.render("index")); //render index.handlebar inside main's body
-        // router.get('/users',(req,res)=>res.render("users"));
+        router.get('/city',(req,res)=>res.render("city"));
         // router.get('/groups',(req,res)=>res.render("groups"));
         return router;
 
