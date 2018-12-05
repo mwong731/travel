5a5ca599c10193e60989f26bc695e5492ec20c1e
ynmwong <melodysweet@gmail.com>
2018-12-05 16:37:35 +0800

    added attraction hb

--- routes/viewRouter.js
+++ routes/viewRouter.js
@@ -6,6 +6,7 @@ module.exports = class ViewRouter{
         const router = express.Router();
         router.get('/',(req,res)=>res.render("index")); //render index.handlebar inside main's body
         router.get('/city',(req,res)=>res.render("city"));
+        router.get('/attraction',(req,res)=>res.render("attraction"));
         // router.get('/groups',(req,res)=>res.render("groups"));
         return router;
 
