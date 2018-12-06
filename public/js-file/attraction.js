1d4c0a02ed07e831abab15b7ae5a822f58571b61
ynmwong <melodysweet@gmail.com>
2018-12-06 12:18:50 +0800

    edit login and signup css

---
+++ public/js-file/attraction.js
@@ -0,0 +1,5 @@
+// Show comment box when "write review" button clicked
+$('#add-comment').on('click',function(){
+    $( "#add-comment-box" ).slideDown( "slow" );
+    $('#add-comment').fadeOut();
+})
