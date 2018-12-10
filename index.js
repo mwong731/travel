e1ffb03b41f1022a9166e95c5214f3bd4031a51d
zwings <zwings@github.com>
2018-12-10 18:52:16 +0800

    change express session to cookie session

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const hb = require('express-handlebars');
 const bodyParser = require('body-parser');
-const expressSession = require('express-session')
+const cookieSession = require('cookie-session');
+
 const passport = require('passport');
 const passportSetup = require('./config/passport-setup');
 const db = require('./config/database-init.js').knex;
 
 //Set up app(unpackage express)
 let app = express();
+
+
+require('dotenv').config();
 
 /*handlebars*/
 
@@ -44,11 +48,11 @@ app.use(express.static("public"));
 //Use body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//Set up express sessions - with the secret to encode the session
-app.use(expressSession({
-    secret: 'thisRealSecret',
-    resave: true,
-    saveUninitialized: true
+
+//Setup coockie session
+app.use(cookieSession({
+    maxAge:60*60*1000,
+    keys:[process.env.SESSION_COOKIE_KEY]
 }));
 
 //Initialize passport
@@ -112,3 +116,12 @@ const options = {
 };
 
 https.createServer(options, app).listen(3000);
+
+
+//Set up express sessions - with the secret to encode the session
+// const expressSession = require('express-session')
+// app.use(expressSession({
+//     secret: 'thisRealSecret',
+//     resave: true,
+//     saveUninitialized: true
+// }));
