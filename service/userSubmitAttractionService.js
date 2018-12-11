1061a3a16a2971a64ec1cad4d59cd90c3b14e7d2
ynmwong <melodysweet@gmail.com>
2018-12-11 14:53:49 +0800

    update profile error

--- service/userSubmitAttractionService.js
+++ service/userSubmitAttractionService.js
@@ -59,10 +59,10 @@ module.exports = class UserSubmitAttractionService {
     }
 
     insertAttraction(cityid, name, type, latitude, longitude, image, description,userid) {
-        console.log("insertin",cityid, name, type, latitude, longitude, image, description)
-        return this.knex('attraction').insert([
+        console.log("insertin",cityid, name, type, latitude, longitude, image, description,userid)
+        return this.knex('attraction').insert(
             { cityid: cityid, name:name, type: type, latitude: latitude, longitude: longitude, image: image , description: description,confirmstatus:"wait",userid:userid}
-        ]);
+        );
     }
     //below function not even test
 }
